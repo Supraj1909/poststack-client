93f17690365ab7ec1be61bd6562589e0eb758296
Supraj1909 <suprajeet22k@gmail.com>
2023-09-05 22:39:24 +0530

    second commit

--- src/scenes/loginPage/index.jsx
+++ src/scenes/loginPage/index.jsx
@@ -13,7 +13,7 @@ const LoginPage = () => {
         textAlign="center"
       >
         <Typography fontWeight="bold" fontSize="32px" color="primary">
-          Sociopedia
+          PostStack
         </Typography>
       </Box>
 
@@ -25,7 +25,7 @@ const LoginPage = () => {
         backgroundColor={theme.palette.background.alt}
       >
         <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
-          Welcome to Socipedia, the Social Media for Sociopaths!
+          Welcome to PostStack, Stack Posts like There's no tomorrow!
         </Typography>
         <Form />
       </Box>
